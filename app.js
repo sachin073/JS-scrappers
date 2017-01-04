7feb7c0a7051ddea169343ed7313b841997dbb7a
sachin073 <verma.sachin@outlook.in>
2017-01-04 23:17:53 +0530

    change 1.0

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 
 /**
  * Module dependencies.
+ *
+ * @description 
+ * simple scrapper based of pure JS using cheerio and NodeJs .
+ * 
+ * @review   
+ * @date 04-jan-2017
+ * 
  */
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var request = require('request');
@@ -48,24 +53,7 @@ app.get('/App1', function (req, res){
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
